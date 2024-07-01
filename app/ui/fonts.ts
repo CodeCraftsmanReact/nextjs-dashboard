b5942bb81bea596c77a6281b5613d2594722f9ef
Joseph Kertz <lonewolfcode2@gmail.com>
2024-07-01 12:15:53 -0500

    nextjsDashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana ,Press_Start_2P } from 'next/font/google';
+
+export const pressStart2p = Press_Start_2P({weight:['400'], subsets: ['latin']});
+export const lusitana = Lusitana({weight: ['400', '700'], subsets:['latin']});
+export const inter = Inter({subsets: ['latin']});
+
